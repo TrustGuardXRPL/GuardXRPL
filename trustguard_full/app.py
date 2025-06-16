5f134e48fe6af52cea996b7fc92455f541077993
TrustGuardXRPL <aaqq61805@gmail.com>
2025-06-16 17:22:17 +0300

    Update app.py

--- trustguard_full/app.py
+++ trustguard_full/app.py
@@ -32,4 +32,6 @@ def report_token():
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
